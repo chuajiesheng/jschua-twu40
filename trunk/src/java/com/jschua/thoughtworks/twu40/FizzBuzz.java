1369b71a8c3f21f0ab2b5ee8e56eec9fe55d68e5
chuajiesheng <chuajiesheng@01936d5e-a163-ba34-92ee-20910d6e2189>
2014-09-11 18:00:56 +0000

    Added FizzBuzz test and refactor

--- trunk/src/java/com/jschua/thoughtworks/twu40/FizzBuzz.java
+++ trunk/src/java/com/jschua/thoughtworks/twu40/FizzBuzz.java
@@ -5,9 +5,22 @@ package com.jschua.thoughtworks.twu40;
  */
 public class FizzBuzz {
     public String print(int n) {
+        String result = printFizz(n) + printBuzz(n);
+        if (result.length() == 0) {
+            return "" + n;
+        } else {
+            return result;
+        }
+    }
+
+    private String printFizz(int n) {
         if (n % 3 == 0) {
             return "Fizz";
         }
+        return "";
+    }
+
+    private String printBuzz(int n) {
         if (n % 5 == 0) {
             return "Buzz";
         }
