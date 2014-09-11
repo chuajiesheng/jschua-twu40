c04ae76e906ec8456ad35c4b16bdd591a967f760
chuajiesheng <chuajiesheng@01936d5e-a163-ba34-92ee-20910d6e2189>
2014-09-11 17:54:16 +0000

    Added Fizz method and test

---
+++ trunk/src/java/com/jschua/thoughtworks/twu40/FizzBuzz.java
@@ -0,0 +1,13 @@
+package com.jschua.thoughtworks.twu40;
+
+/**
+ * Created by chuajiesheng on 12/9/14.
+ */
+public class FizzBuzz {
+    public String print(int n) {
+        if (n % 3 == 0) {
+            return "Fizz";
+        }
+        return "";
+    }
+}
---
+++ trunk/src/java/com/jschua/thoughtworks/twu40/test/FizzBuzzTest.java
@@ -0,0 +1,17 @@
+package com.jschua.thoughtworks.twu40.test;
+
+import com.jschua.thoughtworks.twu40.FizzBuzz;
+import org.junit.Test;
+
+import static junit.framework.Assert.assertEquals;
+
+/**
+ * Created by chuajiesheng on 12/9/14.
+ */
+public class FizzBuzzTest {
+    @Test
+    public void testFizz() throws Exception {
+        String expected = "Fizz";
+        assertEquals(expected, new FizzBuzz().print(3));
+    }
+}
