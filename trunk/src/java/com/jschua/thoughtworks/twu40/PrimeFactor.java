163d4f4d82b93e3d93ac521dc408e43458813351
chuajiesheng <chuajiesheng@01936d5e-a163-ba34-92ee-20910d6e2189>
2014-09-11 18:15:41 +0000

    Added PrimeFactor class and test

---
+++ trunk/src/java/com/jschua/thoughtworks/twu40/PrimeFactor.java
@@ -0,0 +1,29 @@
+package com.jschua.thoughtworks.twu40;
+
+import java.util.ArrayList;
+
+/**
+ * Created by chuajiesheng on 12/9/14.
+ */
+public class PrimeFactor {
+    public String factor(int n) {
+        ArrayList<Integer> list = new ArrayList<Integer>();
+        for (int i = 2; i < n; i++) {
+            if (n % i == 0) {
+                list.add(i);
+            }
+        }
+        return listToString(list);
+    }
+
+    private String listToString(ArrayList<Integer> list) {
+        StringBuilder sb = new StringBuilder();
+        for (int i = 0; i < list.size(); i++) {
+            sb.append(list.get(i));
+            if (i < list.size() - 1) {
+                sb.append(",");
+            }
+        }
+        return sb.toString();
+    }
+}
